b9061ad993855cdac1715d228a93761ec4c0beb1
Lin <lin@lastquestion.org>
2020-04-11 21:25:06 -0700

    prettier

--- content.js
+++ content.js
@@ -1,31 +1,28 @@
 const myAudio = new Audio(chrome.runtime.getURL("ding.mp3"));
 
-
-
 const init = () => {
   const entireBody = document.body.innerText;
-  // So it can match one of those messages: 
-    // - "No delivery windows available", 
-    // - "No attended delivery windows are available"
-    // - "No doorstep delivery windows are available";   
+  // So it can match one of those messages:
+  // - "No delivery windows available",
+  // - "No attended delivery windows are available"
+  // - "No doorstep delivery windows are available";
   if (entireBody.search(/No(.*)delivery windows(.*)available/) !== -1) {
-    console.log('no avail');
+    console.log("no avail");
 
-    chrome.storage.sync.get('refreshInterval', function(data) {
-      console.log(`waiting ${data.refreshInterval} seconds`);       
+    chrome.storage.sync.get("refreshInterval", function (data) {
+      console.log(`waiting ${data.refreshInterval} seconds`);
 
       setTimeout(() => {
         window.location.reload();
       }, data.refreshInterval * 1000);
     });
-  }
-  else {
-    console.log('bing!');
+  } else {
+    console.log("bing!");
     // get the body out for next time. pick slots automatically
     console.log(document.body.innerHTML);
     myAudio.play();
   }
-}
+};
 
 // Set a timeout to let client to render the page
 setTimeout(init, 3000);
